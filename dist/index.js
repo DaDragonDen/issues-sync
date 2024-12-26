db951e9a7127f59fde68f42054a14de17ac32127
Christian Toney <christian@makuwro.com>
2024-12-26 00:01:35 -0500

    Update message on update

--- dist/index.js
+++ dist/index.js
@@ -35362,54 +35362,34 @@ try {
         owner: issuePayload.owner,
         repo: issuePayload.repo
     });
-    // Create a thread referencing the GitHub issue.
-    console.log("Creating Discord thread...");
+    const fieldName = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("field-name", { required: false });
     const discordToken = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("discord-token", { required: true });
     const discordChannelID = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("discord-channel-id", { required: true });
-    const issueCreator = issue.user;
+    const projectIDString = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("project-id", { required: false });
+    const projectID = parseInt(projectIDString, 10);
     const client = new oceanic_js__WEBPACK_IMPORTED_MODULE_0__/* .Client */ .Kje({ auth: `Bot ${discordToken}` });
     await client.restMode(true);
-    const thread = await client.rest.channels.startThreadInThreadOnlyChannel(discordChannelID, {
-        name: `[${issue.number}] ${issue.title}`,
-        message: {
-            embeds: [
-                {
-                    ...issueCreator ? {
-                        author: {
-                            name: `${issueCreator.name ? `${issueCreator.name} (` : ""}${issueCreator?.login}${issueCreator.name ? ")" : ""}`,
-                            url: issueCreator.html_url,
-                            iconURL: issueCreator.avatar_url
-                        }
-                    } : {},
-                    title: issue.title,
-                    url: issue.html_url,
-                    description: issue.body_text
-                }
-            ]
-        }
-    });
-    // Find the project item ID.
-    const projectIDString = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("project-id", { required: false });
-    if (projectIDString) {
-        console.log("Updating Discord thread URL in project...");
-        const projectID = parseInt(projectIDString, 10);
-        const response = await octokit.graphql(`
-      query getItemID($name: String!, $owner: String!, $projectID: Int!, $issueNumber: Int!, $fieldName: String!) {
+    async function getProjectItemQueryResponse() {
+        return await octokit.graphql(`
+      query getProjectItem($name: String!, $owner: String!, $projectID: Int!, $issueNumber: Int!, $fieldName: String!) {
         repository(name: $name, owner: $owner) {
           issue(number: $issueNumber) {
             id
             projectV2(number: $projectID) {
               id
-              field(name: $fieldName) {
-                ... on ProjectV2Field {
-                  id
-                  name
-                  databaseId
-                }
-              }
               items {
                 nodes {
                   id
+                  fieldValueByName(name: $fieldName) {
+                    ... on ProjectV2ItemFieldTextValue {
+                      text
+                      field {
+                        ... on ProjectV2Field {
+                          id
+                        }
+                      }
+                    }
+                  }
                   content {
                     ... on Issue {
                       id
@@ -35426,35 +35406,109 @@ try {
             owner: issuePayload.owner,
             projectID,
             issueNumber: issuePayload.number,
-            fieldName: _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("field-name", { required: true })
+            fieldName
         });
-        console.log(JSON.stringify(response));
-        const targetNodeID = response.repository.issue.id;
-        const projectNodeID = response.repository.issue.projectV2.id;
-        const nodes = response.repository.issue.projectV2.items.nodes;
-        const itemID = nodes.find((node) => node.content.id === targetNodeID).id;
-        const fieldID = response.repository.issue.projectV2.field.id;
-        // Set the thread ID on the issue.
-        await octokit.graphql(`
-      mutation setItemFields($projectNodeID: ID!, $itemID: ID!, $fieldID: ID!) {
-        updateProjectV2ItemFieldValue(
-          input: {
-            projectId: $projectNodeID
-            itemId: $itemID
-            fieldId: $fieldID
-            value: {
-              text: "https://discord.com/channels/${thread.guildID}/${thread.id}"
-            }
-          }
-        ) {
-          projectV2Item {
-            id
-          }  
-        }
-      }
-    `, { projectNodeID, itemID, fieldID });
     }
-    console.log("Successfully created issue.");
+    switch (_actions_github__WEBPACK_IMPORTED_MODULE_2__.context.action) {
+        case "opened":
+        case "updated": {
+            if (!fieldName) {
+                console.warn("A field name must be provided to search for an existing discussion thread on Discord.");
+                break;
+            }
+            // Get the discussion link from GitHub.
+            const response = await getProjectItemQueryResponse();
+            const nodes = response.repository.issue.projectV2.items.nodes;
+            const targetNodeID = response.repository.issue.id;
+            const item = nodes.find((node) => node.content.id === targetNodeID);
+            const discussionLink = item?.fieldValueByName.text;
+            const discordMessage = {
+                embeds: [
+                    {
+                        ...issue.user?.login ? {
+                            author: {
+                                name: `${issue.user.name ? `${issue.user.name} (` : ""}${issue.user.login}${issue.user.name ? ")" : ""}`,
+                                url: issue.user.html_url,
+                                iconURL: issue.user.avatar_url
+                            }
+                        } : {},
+                        title: issue.title,
+                        url: issue.html_url,
+                        description: issue.body_text,
+                        fields: [
+                            {
+                                name: "Repository",
+                                value: `[${issuePayload.owner}/${issuePayload.repo}](${issue.repository_url})`,
+                            },
+                            {
+                                name: "Issue",
+                                value: `${issue.number}`,
+                            }
+                        ]
+                    }
+                ]
+            };
+            if (discussionLink) {
+                // Verify the link is set up correctly.
+                const discordLinkRegex = /discordapp\.com\/channels\/\d+\/(?<channelID>\d+)\/(?<messageID>\d+)/g;
+                const discordThreadIDMatch = discussionLink.match(discordLinkRegex);
+                const channelID = discordThreadIDMatch?.groups?.channelID;
+                const messageID = discordThreadIDMatch?.groups?.messageID;
+                if (!channelID)
+                    throw new Error("Channel ID not provided in link.");
+                if (!messageID)
+                    throw new Error("Thread ID not provided in link.");
+                // Edit the existing message.
+                const message = await client.rest.channels.getMessage(channelID, messageID);
+                await message.edit(discordMessage);
+            }
+            else {
+                // Create a discussion link since it doesn't exist.
+                // Create a thread referencing the GitHub issue.
+                console.log("Creating Discord thread...");
+                const thread = await client.rest.channels.startThreadInThreadOnlyChannel(discordChannelID, {
+                    name: issue.title,
+                    message: discordMessage
+                });
+                const threadMessageID = thread.lastMessageID;
+                // Find the project item ID.
+                if (projectID) {
+                    console.log("Updating Discord thread URL in project...");
+                    const response = await getProjectItemQueryResponse();
+                    const targetNodeID = response.repository.issue.id;
+                    const projectNodeID = response.repository.issue.projectV2.id;
+                    const nodes = response.repository.issue.projectV2.items.nodes;
+                    const item = nodes.find((node) => node.content.id === targetNodeID);
+                    const itemID = item?.id;
+                    const fieldID = item?.fieldValueByName.field.id;
+                    // Set the thread ID on the issue.
+                    await octokit.graphql(`
+            mutation setItemFields($projectNodeID: ID!, $itemID: ID!, $fieldID: ID!, $threadJumpLink: String!) {
+              updateProjectV2ItemFieldValue(
+                input: {
+                  projectId: $projectNodeID
+                  itemId: $itemID
+                  fieldId: $fieldID
+                  value: {
+                    text: $threadJumpLink
+                  }
+                }
+              ) {
+                projectV2Item {
+                  id
+                }  
+              }
+            }
+          `, { projectNodeID, itemID, fieldID, threadJumpLink: `https://discordapp.com/channels/${thread.guildID}/${discordChannelID}/${threadMessageID}` });
+                }
+                console.log("Successfully created issue.");
+                break;
+            }
+            break;
+        }
+        default:
+            break;
+    }
 }
 catch (error) {
     console.error(error);
--- src/index.ts
+++ src/index.ts
@@ -22,62 +22,31 @@ try {
     repo: issuePayload.repo
   });
 
-  // Create a thread referencing the GitHub issue.
-  console.log("Creating Discord thread...");
+  const fieldName = core.getInput("field-name", {required: false});
   const discordToken = core.getInput("discord-token", {required: true});
   const discordChannelID = core.getInput("discord-channel-id", {required: true});
-  const issueCreator = issue.user;
+  const projectIDString = core.getInput("project-id", {required: false});
+  const projectID = parseInt(projectIDString, 10);
   const client = new Client({auth: `Bot ${discordToken}`});
   await client.restMode(true);
-  
-  const thread = await client.rest.channels.startThreadInThreadOnlyChannel(discordChannelID, {
-    name: issue.title,
-    message: {
-      embeds: [
-        {
-          ... issueCreator ? {
-            author: {
-              name: `${issueCreator.name ? `${issueCreator.name} (` : ""}${issueCreator?.login}${issueCreator.name ? ")" : ""}`,
-              url: issueCreator.html_url,
-              iconURL: issueCreator.avatar_url
-            }
-          } : {},
-          title: issue.title,
-          url: issue.html_url,
-          description: issue.body_text,
-          fields: [
-            {
-              name: "Repository",
-              value: `[${issuePayload.owner}/${issuePayload.repo}](${issue.repository_url})`,
-            },
-            {
-              name: "Issue",
-              value: `${issue.number}`,
-            }
-          ]
-        }
-      ]
-    }
-  });
 
-  // Find the project item ID.
-  const projectIDString = core.getInput("project-id", {required: false});
-  if (projectIDString) {
+  async function getProjectItemQueryResponse() {
 
-    console.log("Updating Discord thread URL in project...");
-    const projectID = parseInt(projectIDString, 10);
-    const response = await octokit.graphql<{
+    return await octokit.graphql<{
       repository: {
         issue: {
           id: string;
           projectV2: {
             id: string;
-            field: {
-              id: number;
-            };
             items: {
               nodes: {
                 id: string,
+                fieldValueByName: {
+                  text: string;
+                  field: {
+                    id: string;
+                  }
+                }
                 content: {
                   id: string
                 }
@@ -87,22 +56,25 @@ try {
         }
       }
     }>(`
-      query getItemID($name: String!, $owner: String!, $projectID: Int!, $issueNumber: Int!, $fieldName: String!) {
+      query getProjectItem($name: String!, $owner: String!, $projectID: Int!, $issueNumber: Int!, $fieldName: String!) {
         repository(name: $name, owner: $owner) {
           issue(number: $issueNumber) {
             id
             projectV2(number: $projectID) {
               id
-              field(name: $fieldName) {
-                ... on ProjectV2Field {
-                  id
-                  name
-                  databaseId
-                }
-              }
               items {
                 nodes {
                   id
+                  fieldValueByName(name: $fieldName) {
+                    ... on ProjectV2ItemFieldTextValue {
+                      text
+                      field {
+                        ... on ProjectV2Field {
+                          id
+                        }
+                      }
+                    }
+                  }
                   content {
                     ... on Issue {
                       id
@@ -119,38 +91,130 @@ try {
       owner: issuePayload.owner,
       projectID,
       issueNumber: issuePayload.number,
-      fieldName: core.getInput("field-name", {required: true})
+      fieldName
     });
-    console.log(JSON.stringify(response));
-    const targetNodeID = response.repository.issue.id;
-    const projectNodeID = response.repository.issue.projectV2.id;
-    const nodes = response.repository.issue.projectV2.items.nodes;
-    const itemID = nodes.find((node) => node.content.id === targetNodeID)!.id;
-    const fieldID = response.repository.issue.projectV2.field.id;
-
-    // Set the thread ID on the issue.
-    await octokit.graphql(`
-      mutation setItemFields($projectNodeID: ID!, $itemID: ID!, $fieldID: ID!) {
-        updateProjectV2ItemFieldValue(
-          input: {
-            projectId: $projectNodeID
-            itemId: $itemID
-            fieldId: $fieldID
-            value: {
-              text: "https://discord.com/channels/${thread.guildID}/${thread.id}"
-            }
-          }
-        ) {
-          projectV2Item {
-            id
-          }  
-        }
-      }
-    `, {projectNodeID, itemID, fieldID});
 
   }
 
-  console.log("Successfully created issue.");
+  switch (github.context.action) {
+
+    case "opened":
+    case "updated": {
+
+      if (!fieldName) {
+
+        console.warn("A field name must be provided to search for an existing discussion thread on Discord.");
+        break;
+
+      }
+
+      // Get the discussion link from GitHub.
+      const response = await getProjectItemQueryResponse();
+      const nodes = response.repository.issue.projectV2.items.nodes;
+      const targetNodeID = response.repository.issue.id;
+      const item = nodes.find((node) => node.content.id === targetNodeID);
+      const discussionLink = item?.fieldValueByName.text;
+      const discordMessage = {
+        embeds: [
+          {
+            ... issue.user?.login ? {
+              author: {
+                name: `${issue.user.name ? `${issue.user.name} (` : ""}${issue.user.login}${issue.user.name ? ")" : ""}`,
+                url: issue.user.html_url,
+                iconURL: issue.user.avatar_url
+              }
+            } : {},
+            title: issue.title,
+            url: issue.html_url,
+            description: issue.body_text,
+            fields: [
+              {
+                name: "Repository",
+                value: `[${issuePayload.owner}/${issuePayload.repo}](${issue.repository_url})`,
+              },
+              {
+                name: "Issue",
+                value: `${issue.number}`,
+              }
+            ]
+          }
+        ]
+      };
+
+      if (discussionLink) {
+
+        // Verify the link is set up correctly.
+        const discordLinkRegex = /discordapp\.com\/channels\/\d+\/(?<channelID>\d+)\/(?<messageID>\d+)/g;
+        const discordThreadIDMatch = discussionLink.match(discordLinkRegex);
+        const channelID = discordThreadIDMatch?.groups?.channelID;
+        const messageID = discordThreadIDMatch?.groups?.messageID;
+        if (!channelID) throw new Error("Channel ID not provided in link.");
+        if (!messageID) throw new Error("Thread ID not provided in link.");
+
+        // Edit the existing message.
+        const message = await client.rest.channels.getMessage(channelID, messageID);
+        await message.edit(discordMessage);
+
+      } else {
+
+        // Create a discussion link since it doesn't exist.
+        // Create a thread referencing the GitHub issue.
+        console.log("Creating Discord thread...");
+
+        const thread = await client.rest.channels.startThreadInThreadOnlyChannel(discordChannelID, {
+          name: issue.title,
+          message: discordMessage
+        });
+
+        const threadMessageID = thread.lastMessageID;
+
+        // Find the project item ID.
+        if (projectID) {
+
+          console.log("Updating Discord thread URL in project...");
+          const response = await getProjectItemQueryResponse();
+          const targetNodeID = response.repository.issue.id;
+          const projectNodeID = response.repository.issue.projectV2.id;
+          const nodes = response.repository.issue.projectV2.items.nodes;
+          const item = nodes.find((node) => node.content.id === targetNodeID);
+          const itemID = item?.id;
+          const fieldID = item?.fieldValueByName.field.id;
+
+          // Set the thread ID on the issue.
+          await octokit.graphql(`
+            mutation setItemFields($projectNodeID: ID!, $itemID: ID!, $fieldID: ID!, $threadJumpLink: String!) {
+              updateProjectV2ItemFieldValue(
+                input: {
+                  projectId: $projectNodeID
+                  itemId: $itemID
+                  fieldId: $fieldID
+                  value: {
+                    text: $threadJumpLink
+                  }
+                }
+              ) {
+                projectV2Item {
+                  id
+                }  
+              }
+            }
+          `, {projectNodeID, itemID, fieldID, threadJumpLink: `https://discordapp.com/channels/${thread.guildID}/${discordChannelID}/${threadMessageID}`});
+
+        }
+
+        console.log("Successfully created issue.");
+        break;
+
+      }
+      
+      break;
+
+    }
+
+    default:
+      break;
+
+  }
 
 } catch (error) {
 
