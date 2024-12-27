8d3c2ef59bb4e2613de9214b5b86dda4a53056f0
Christian Toney <christian@makuwro.com>
2024-12-27 01:09:38 -0500

    fix map

--- dist/index.js
+++ dist/index.js
@@ -35416,7 +35416,7 @@ try {
     }
     const githubActionType = _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.payload.action;
     const { fieldText: discussionLink, issueType } = await getFieldTextAndIssueType();
-    const discordUserMap = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("discord-user-map", { required: false })?.split("\n").map((row) => row.split("="));
+    const discordUserMap = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput("discord-user-map", { required: false })?.split("\n").map((row) => row.replaceAll("\n", "").split("="));
     const discordMessage = {
         embeds: [
             {
@@ -35446,7 +35446,7 @@ try {
                             name: `Assignee${issue.assignees.length > 1 ? "s" : ""}`,
                             value: issue.assignees.map((assignee, index) => {
                                 const connection = discordUserMap?.find((pair) => parseInt(pair[0], 10) === assignee.id);
-                                const discordUserID = parseInt(connection?.[1] ?? "", 10);
+                                const discordUserID = connection?.[1];
                                 return `${discordUserID ? `<@${discordUserID}>` : `[@${assignee.login}](${assignee.html_url})`}${issue.assignees?.[index + 1] ? "\n" : ""}`;
                             }).join()
                         }
--- src/index.ts
+++ src/index.ts
@@ -98,7 +98,7 @@ try {
   const githubActionType = github.context.payload.action;
 
   const { fieldText: discussionLink, issueType } = await getFieldTextAndIssueType();
-  const discordUserMap = core.getInput("discord-user-map", {required: false})?.split("\n").map((row) => row.split("="));
+  const discordUserMap = core.getInput("discord-user-map", {required: false})?.split("\n").map((row) => row.replaceAll("\n", "").split("="));
   const discordMessage = {
     embeds: [
       {
@@ -129,7 +129,7 @@ try {
               value: issue.assignees.map((assignee, index) => {
 
                 const connection = discordUserMap?.find((pair) => parseInt(pair[0], 10) === assignee.id);
-                const discordUserID = parseInt(connection?.[1] ?? "", 10);
+                const discordUserID = connection?.[1];
                 return `${discordUserID ? `<@${discordUserID}>` : `[@${assignee.login}](${assignee.html_url})`}${issue.assignees?.[index + 1] ? "\n" : ""}`
 
               }).join()
